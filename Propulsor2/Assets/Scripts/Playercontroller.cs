d6963380a3ef3f102de99660c9b18d733bdacccd
YANI018 <yani.chasco@gmail.com>
2022-10-06 12:29:19 +0100

    arreglado gasolina

--- Propulsor2/Assets/Scripts/Playercontroller.cs
+++ Propulsor2/Assets/Scripts/Playercontroller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Playercontroller : MonoBehaviour
 {
@@ -14,38 +15,44 @@ public class Playercontroller : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI labelFuel;
 
-    
+
     float gasolinaActual = 100f;
 
 
     [SerializeField]
     GameObject prefabParticles;
 
-    void Start ()
+    void Start()
     {
 
         gasolinaActual = 100f;
         body = GetComponent<Rigidbody2D>();
-    
-    
-    }
-        
-      
-         
-    
 
-   
+
+    }
+
+
+
+
+
+
     void Update()
     {
 
 
-        
-        direction.x = Input.GetAxis ("Horizontal") * Time.deltaTime * IMPULSE;
-        direction.y = Input.GetAxis ("Vertical") * Time.deltaTime * IMPULSE;
 
-        gasolinaActual = gasolinaActual - 0.5f * Time.deltaTime;
-        labelFuel.text = gasolinaActual. ToString("00") + "%";
+        direction.x = Input.GetAxis("Horizontal") * Time.deltaTime * IMPULSE;
+        direction.y = Input.GetAxis("Vertical") * Time.deltaTime * IMPULSE;
 
+        gasolinaActual = gasolinaActual - 1f * Time.deltaTime;
+        labelFuel.text = gasolinaActual.ToString("00") + "%";
+
+
+        if (gasolinaActual <= 0.0f)
+        {
+            
+            this.enabled = false;
+        }
 
     }
     private void FixedUpdate()
@@ -53,27 +60,33 @@ public class Playercontroller : MonoBehaviour
 
         body.AddForce(direction, ForceMode2D.Impulse);
     }
-      private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
 
-    { 
-        if ( collision.tag =="Fuel")
+    {
+        if (collision.tag == "fuel")
         {
             gasolinaActual += 10f;
             if (gasolinaActual > 100f)
-            { 
-                gasolinaActual = 100f; 
-            
+            {
+                gasolinaActual = 100f;
+
             }
 
-            Instantiate(prefabParticles, collision.transform.position, collision.transform.
-                rotation);
+            Instantiate(prefabParticles, collision.transform.position, collision.transform.rotation);
             Destroy(collision.gameObject);
 
 
         }
     }
+    public void ClickEnBoton()
+    {
+        
+        Debug.Log("ha clicado");
+        SceneManager.LoadScene(0);        
     
+    }
 }
+
 
     
 
